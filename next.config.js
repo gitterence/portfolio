ee2087718b994b28963146337153ce4279ab0d63
gitterence <terencejye@gmail.com>
2023-05-28 21:52:13 -0700

    fix: change nextConfig to have output export (for static)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    images: {
-      unoptimized: true,
-    },
-  },
+  output: "export",
 };
 
 module.exports = nextConfig;
