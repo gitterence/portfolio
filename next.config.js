edfb6fd044358d41681c4dd781d6d578a4403ba0
gitterence <terencejye@gmail.com>
2023-09-03 15:45:15 -0700

    fix: add env to differentiate prod or local

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  output: "export",
+  // Static exports
+  // Read more: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: isProd ? "export" : "standalone",
+
+  // Note: Image Optimization using the default loader is not compatible with `{ output: 'export' }`.
+  // Read more: https://nextjs.org/docs/messages/export-image-api
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
