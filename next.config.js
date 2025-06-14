85713766daac513e1123e2ddfe2173a168493e4a
terence <88063046+gitterence@users.noreply.github.com>
2025-06-14 21:47:59 +0000

    fix: add basePath in nextjs config and comment out next export command

--- next.config.js
+++ next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
   // Read more: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
   output: isProd ? "export" : "standalone",
 
+  // By default, Next.js maps all static assets the domain. 
+  // This means that the favicon.ico file will be resolved to gitterence.github.io/favicon.ico 
+  // instead of gitterence.github.io/portfolio/favicon.icon.
+  basePath: isProd ? '/portfolio' : '', 
+
   // Note: Image Optimization using the default loader is not compatible with `{ output: 'export' }`.
   // If using `next/image`, it might need to unoptimize images for static export
   // Read more: https://nextjs.org/docs/messages/export-image-api
